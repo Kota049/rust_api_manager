3f58e53370152f65430ec64dd75bf979e0eec06e
Kota <yuzukh0211@gmail.com>
2023-08-15 16:14:16 +0900

    red

--- api_manager/src/objects/api/tests.rs
+++ api_manager/src/objects/api/tests.rs
@@ -13,3 +13,17 @@ fn create_valid_url(){
     // then
     assert_eq!(result, Ok(Api { url: String::from("/api/v1/hoge"), method: String::from("GET"), parameter: Vec::new() }));
 }
+
+#[test]
+fn url_heading_non_slash_return_error(){
+    // given
+    let url = String::from("api/v1/hoge");
+    let method = String::from("GET");
+    let parameter = Vec::new();
+
+    // when
+    let result = Api::new(url,method,parameter);
+
+    // then
+    assert_eq!(result, Ok(String::from("不正なURLです(url should be start with /)")));
+}
