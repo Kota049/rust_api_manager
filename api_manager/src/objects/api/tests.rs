79332243f29d540b52d273bd35640ab1ae2ebd9b
Kota <yuzukh0211@gmail.com>
2023-08-15 16:52:24 +0900

    red

--- api_manager/src/objects/api/tests.rs
+++ api_manager/src/objects/api/tests.rs
@@ -41,3 +41,17 @@ fn invalid_method_return_error() {
     // then
     assert_eq!(result, Err(String::from("Invalid Method(method should be http method[GET,POST,PUT,DELETE])")));
 }
+
+#[test]
+fn lower_method_return_ok() {
+    // given
+    let url = String::from("/api/v1/hoge");
+    let method = String::from("get");
+    let parameter = Vec::new();
+
+    // when
+    let result = Api::new(url, method, parameter);
+
+    // then
+    assert_eq!(result, Ok(Api { url: String::from("/api/v1/hoge"), method: String::from("GET"), parameter: Vec::new() }));
+}
