4f94cece8577f896026f987dcf80534e23da99de
Kota <yuzukh0211@gmail.com>
2023-08-15 16:19:40 +0900

    refactor move deal to function

--- api_manager/src/objects/api.rs
+++ api_manager/src/objects/api.rs
@@ -1,22 +1,27 @@
 #[cfg(test)]
 pub mod tests;
 
-#[derive(Debug,PartialEq)]
-pub struct Api{
-    url:String,
-    method:String,
-    parameter:Vec<String>
+#[derive(Debug, PartialEq)]
+pub struct Api {
+    url: String,
+    method: String,
+    parameter: Vec<String>,
 }
 
 impl Api {
-    pub fn new(url:String,method:String,parameter:Vec<String>)->Result<Api,String>{
-        if !url.starts_with('/') {
-            return Err(String::from("不正なURLです(url should be start with /)"))
-        }
+    pub fn new(url: String, method: String, parameter: Vec<String>) -> Result<Api, String> {
+        validate_url(&url)?;
         Ok(Api {
             url,
             method,
             parameter,
         })
     }
+}
+
+fn validate_url(url: &str) -> Result<(), String> {
+    if !url.starts_with('/') {
+        return Err(String::from("不正なURLです(url should be start with /)"));
+    }
+    Ok(())
 }
--- api_manager/src/objects/api/tests.rs
+++ api_manager/src/objects/api/tests.rs
@@ -1,28 +1,28 @@
 use super::*;
 
 #[test]
-fn create_valid_url(){
+fn create_valid_url() {
     // given
     let url = String::from("/api/v1/hoge");
     let method = String::from("GET");
     let parameter = Vec::new();
 
     // when
-    let result = Api::new(url,method,parameter);
+    let result = Api::new(url, method, parameter);
 
     // then
     assert_eq!(result, Ok(Api { url: String::from("/api/v1/hoge"), method: String::from("GET"), parameter: Vec::new() }));
 }
 
 #[test]
-fn url_heading_non_slash_return_error(){
+fn url_heading_non_slash_return_error() {
     // given
     let url = String::from("api/v1/hoge");
     let method = String::from("GET");
     let parameter = Vec::new();
 
     // when
-    let result = Api::new(url,method,parameter);
+    let result = Api::new(url, method, parameter);
 
     // then
     assert_eq!(result, Err(String::from("不正なURLです(url should be start with /)")));
