613eb677cdade9054df31f8153212b2a434a43ae
Kota <yuzukh0211@gmail.com>
2023-08-15 15:00:22 +0900

    red

--- api_manager/src/store/tests.rs
+++ api_manager/src/store/tests.rs
@@ -19,3 +19,27 @@ async fn store_json_string() {
     let content = fs::read_to_string(FILE_NAME).unwrap();
     assert_eq!(content, serde_json::to_string_pretty(&value).unwrap() + "\n");
 }
+
+#[tokio::test]
+async fn store_json_string_include_multi_api() {
+    // given
+    let value = json!([
+        {
+        "url":"/api/v1/hoge",
+        "method":"GET",
+        "parameter":["email"],
+        },
+        {
+        "url":"/api/v1/hoge2",
+        "method":"GET",
+        "parameter":["email2"],
+        },
+    ]);
+
+    // when
+    store(&value).await.unwrap();
+
+    // then
+    let content = fs::read_to_string(FILE_NAME).unwrap();
+    assert_eq!(content, serde_json::to_string_pretty(&value).unwrap() + "\n");
+}
