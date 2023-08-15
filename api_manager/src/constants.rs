9aac861c117a6206e30a5aa731b9a1e8b276b6dd
Kota <yuzukh0211@gmail.com>
2023-08-15 16:50:28 +0900

    refactor extract method

--- api_manager/src/constants.rs
+++ api_manager/src/constants.rs
@@ -1,1 +1,8 @@
 pub const FILE_NAME: &'static str = "api_manager.json";
+
+pub const HTTP_METHODS: [&str; 4] = [
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE"
+];
--- api_manager/src/objects/api.rs
+++ api_manager/src/objects/api.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 pub mod tests;
 
+use super::super::constants::HTTP_METHODS;
+
 #[derive(Debug, PartialEq)]
 pub struct Api {
     url: String,
@@ -11,17 +13,22 @@ pub struct Api {
 impl Api {
     pub fn new(url: String, method: String, parameter: Vec<String>) -> Result<Api, String> {
         validate_url(&url)?;
-        if &method == "hoge" {
-            return Err(String::from("Invalid Method(method should be http method[GET,POST,PUT,DELETE])"));
-        }
+        validate_method(&method)?;
         Ok(Api {
             url,
             method,
             parameter,
         })
     }
+
 }
 
+fn validate_method(method: &str)->Result<(),String> {
+    if !HTTP_METHODS.iter().any(|e| e == &method) {
+        return Err(String::from("Invalid Method(method should be http method[GET,POST,PUT,DELETE])"));
+    }
+    Ok(())
+}
 fn validate_url(url: &str) -> Result<(), String> {
     if !url.starts_with('/') {
         return Err(String::from("Invalid URL(url should be start with /)"));
